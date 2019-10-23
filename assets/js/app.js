13ab3a447b5b3c6abc11a57c33c2489b6083a38a
haider <haidertiger10@gmail.com>
2019-10-24 00:10:39 +0500

    update-menu-small

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,14 @@
 let picon = document.querySelector('.icon');
 let pmenu = document.querySelector('.menu');
 let pnavbar = document.querySelector('.navbar');
+let body = document.getElementsByTagName("body")[0];
 
 
 
 picon.addEventListener('click', function(){
     pnavbar.classList.toggle('display-nav');
     pmenu.classList.toggle('menu-active');
+    body.style.overflow = "hidden";
 })
 
 
